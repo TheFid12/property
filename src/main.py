ee938fc3e73f94224b70c3096bb054b44c27f165
TheFid12 <zhenya.bytchkov@mail.ru>
2026-03-07 18:53:36 +0300

    requirements.txt + logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+from loguru import logger
 from src.source import (
     FileTaskSource,
     GeneratorTaskSource,
     APITaskSource,
-    collect_all_tasks,
+    collect_all,
+)
+
+logger.remove()
+logger.add(
+    "log.txt",
+    format="{time:HH:mm:ss} - {level} - {message}",
+    level="INFO",
+    mode="a",
+    encoding="utf-8",
 )
 
 
@@ -11,14 +21,14 @@ def main() -> None:
     Точка входа в модуль.
     В sources указываются источники задач.
     """
+    logger.info("Начало получения задач из источников.")
     sources = [
         FileTaskSource("tasks.json"),
         GeneratorTaskSource(count=3, prefix="task"),
         APITaskSource([{"id": "api_task_1", "payload": "from api"}])
     ]
-
-    tasks = collect_all_tasks(sources)
-
+    tasks = collect_all(sources)
+    logger.info(f"Сбор задач завершен. Всего: {len(tasks)}")
     for task in tasks:
         print(f"[{task.id}] {task.payload}")
 
